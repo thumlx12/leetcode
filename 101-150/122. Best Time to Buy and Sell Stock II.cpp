ca51e60b319a1b2517278d7e3823a40d64cab290
maolixuan <maolixuan@sensetime.com>
2017-05-05 19:57:25 +0800

    LC122

---
+++ 101-150/122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,30 @@
+//
+// Created by sensetime on 5/5/17.
+//
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <climits>
+#include <math.h>
+#include <algorithm>
+#include <stack>
+#include <unordered_map>
+#include <stdlib.h>
+#include <time.h>
+#include <queue>
+
+using namespace std;
+
+int maxProfit(vector<int> &prices) {
+    if (prices.size() <= 1) {
+        return 0;
+    }
+    int maxProfit = 0;
+    for (int i = 0; i < prices.size() - 1; ++i) {
+        if (prices[i + 1] > prices[i]) {
+            maxProfit += (prices[i + 1] - prices[i]);
+        }
+    }
+    return maxProfit;
+}
