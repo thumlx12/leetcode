6f83b74d21879ae64ea3baa1a4070eb0e285304f
maolixuan <maolixuan@sensetime.com>
2017-05-23 18:59:38 +0800

    141

---
+++ 101-150/141. Linked List Cycle.cpp
@@ -0,0 +1,43 @@
+//
+// Created by sensetime on 5/23/17.
+//
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <climits>
+#include <math.h>
+#include <algorithm>
+#include <stack>
+#include <unordered_map>
+#include <stdlib.h>
+#include <time.h>
+#include <queue>
+#include <bits/unordered_set.h>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution141 {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) {
+            return false;
+        }
+        bool hasCycle = false;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
